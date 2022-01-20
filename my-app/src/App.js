44f41d4add5ee52260378a39dfce194359be5796
Mdnd143 <mohd7210357259@gmail.com>
2022-01-20 12:18:56 +0530

    working mode

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,21 @@
 import './App.css';
-
 import React, { Component } from 'react'
-import NavBar from './components/NavBar';
-import News from './components/News';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import Navbar from './Navbar';
+import News from './News';
+;
 
-export default class App extends Component {
+
+export class App extends Component {
+
 
   render() {
     return (
       <div>
-        <Router>
-        <NavBar/> 
-        <Routes>
-          <Route exact path="/"><News key="general" pageSize={5} country="in" category="general"/></Route> 
-          <Route exact path="/business"><News key="business" pageSize={5} country="in" category="business"/></Route> 
-          <Route exact path="/entertainment"><News key="entertainment" pageSize={5} country="in" category="entertainment"/></Route> 
-          <Route exact path="/general"><News key="general" pageSize={5} country="in" category="general"/></Route> 
-          <Route exact path="/health"><News key="health" pageSize={5} country="in" category="health"/></Route> 
-          <Route exact path="/science"><News key="science" pageSize={5} country="in" category="science"/></Route> 
-          <Route exact path="/sports"><News key="sports" pageSize={5} country="in" category="sports"/></Route> 
-          <Route exact path="/technology"><News key="technology" pageSize={5} country="in" category="technology"/></Route> 
-        </Routes>
-        </Router>
+        <Navbar/>
+        <News  key="general" pageSize={10} country="us" category="general"/>
       </div>
     )
-  }
 }
+}
+
+export default App;
